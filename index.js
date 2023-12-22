ff8d7efb7d056ef0bdf375de0399eebd57a3f3af
Sahil <garg07825@gmail.com>
2023-12-22 10:47:06 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import { config } from "dotenv";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import router from "./routes/user.js";
+import userRouter from "./routes/user.js";
+import urlRouter from "./routes/Url.js";
 import { errorMiddleware } from "./middlewares/error.js";
+import isAuthenticated from "./middlewares/Authenticated.js";
 export const app = express();
 config({
   path: "./.env",
 });
 
 // middlewares
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.use("/user", router);
+app.use("/user", userRouter);
+app.use("/url", isAuthenticated, urlRouter);
 
 // error handler middleware
 app.use(errorMiddleware);
--- middlewares/Authenticated.js
+++ middlewares/Authenticated.js
@@ -1,16 +1,18 @@
-import User from "../models/user";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
-export const isAuthenticated = async (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token)
     return res.status(404).json({
       success: false,
-      message: "Login First ",
+      message: "Login First",
     });
 
   const decodedData = jwt.verify(token, process.env.jwt_Secret);
   req.user = await User.findById(decodedData._id);
   next();
 };
+
+export default isAuthenticated;
