5b6c018f6dd62768b1d5b7b549421d690dd72461
Sahil <garg07825@gmail.com>
2023-12-22 08:45:51 +0530

    assiign a router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import router from "./routes/user";
 export const app = express();
 config({
   path: "./.env",
@@ -10,3 +11,6 @@ config({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+// routes
+app.use("/user", router);
