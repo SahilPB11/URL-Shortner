82692659f18c4f3952a83be4cd5fa260308d810e
Sahil <garg07825@gmail.com>
2023-12-22 08:41:40 +0530

    installed cookie-parser

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
-
+import cookieParser from "cookie-parser";
 export const app = express();
 config({
   path: "./.env",
 });
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
